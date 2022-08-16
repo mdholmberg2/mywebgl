d00e186b200541a7213b05d9ac7f98ee77d8779d
DonHolmberg <mdholmberg2@gmail.com>
2022-08-16 07:40:34 -0500

    addsContent

--- WebGLProgrammingGuide/Plot.js
+++ WebGLProgrammingGuide/Plot.js
@@ -1,4 +1,4 @@
-// ManyPoints.js
+// Plot.js
 
 // vertex shader program
 var VSHADER_SOURCE = 
@@ -13,7 +13,7 @@ var VSHADER_SOURCE =
 var FSHADER_SOURCE =
     "void main() {\n" +
     "  gl_FragColor = vec4(1.0, 1.0, 1.0, 1.0);\n" + // set the color
-    "}\n";
+    "}\n"
 
 
 function main() {
@@ -39,40 +39,32 @@ function main() {
         return;
     }
 
-    // get storage location of attribute variable
-    //var a_Position = gl.getAttribLocation(gl.program, "a_Position");
-    //var a_PointSize = gl.getAttribLocation(gl.program, "a_PointSize");
-
-    // pass vertex data to shader
-    //var pointSize = 10.0;
-
-    //gl.vertexAttrib1f(a_PointSize, pointSize);
-
     // specify the color for clearing the canvas
-    gl.clearColor(0.0, 0.0, 0.0, 1.0);  // black
+    gl.clearColor(0.7, 0.7, 0.7, 1.0);  // gray
     
     // clear the canvas
     gl.clear(gl.COLOR_BUFFER_BIT);
 
     // draw 3 points
-    gl.drawArrays(gl.POINTS, 0, n);
+    gl.drawArrays(gl.LINES, 0, n)
+
+
+    // points
+    n = initVertexBuffers2(gl);
+    if (n < 0) {
+        console.log("failed to init buffers");
+        return;
+    }
+
+    gl.drawArrays(gl.POINTS, 0, n)
 
 }
 
 
 function initVertexBuffers(gl) {
-
-    var verticies = new Float32Array(1000);     // items of x,y
-    for ( i = 0; i < 1000; i++) {
-
-        let x = Math.random();
-        if (Math.random() > 0.5) {
-            x = x * -1;
-        }
-
-
-        verticies[i] = x;
-    }
+    var verticies = new Float32Array([      // items of x,y
+        -1.0, 0.0,  1.0, 0.0,   0.0, 1.0,   0.0, -1.0
+    ])
 
     var n = verticies.length / 2; //number of vertices
 
@@ -92,7 +84,43 @@ function initVertexBuffers(gl) {
     var a_Position = gl.getAttribLocation(gl.program, "a_Position");
     var a_PointSize = gl.getAttribLocation(gl.program, "a_PointSize");
 
-    var pointSize = 5.0;
+    var pointSize = 10.0;
+
+    gl.vertexAttrib1f(a_PointSize, pointSize);
+
+    // assign buffer object to position variable
+    gl.vertexAttribPointer(a_Position, 2, gl.FLOAT, false, 0, 0);
+
+    //enable the assignment to a_Position variable
+    gl.enableVertexAttribArray(a_Position);
+
+    return n;
+}
+
+function initVertexBuffers2(gl) {
+    var verticies = new Float32Array([      // items of x,y
+        0.0, 0.0,   -0.5, 0.7,  0.5, -0.3
+    ])
+
+    var n = verticies.length / 2; //number of vertices
+
+
+    // create buffer object
+    var vertexBuffer = gl.createBuffer();
+    if (!vertexBuffer) {
+        console.log("failed to create buffer object");
+        return -1;
+    }
+
+    // bind buffer object to target
+    gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
+    // write data into buffer object
+    gl.bufferData(gl.ARRAY_BUFFER, verticies, gl.STATIC_DRAW);
+
+    var a_Position = gl.getAttribLocation(gl.program, "a_Position");
+    var a_PointSize = gl.getAttribLocation(gl.program, "a_PointSize");
+
+    var pointSize = 10.0;
 
     gl.vertexAttrib1f(a_PointSize, pointSize);
 
